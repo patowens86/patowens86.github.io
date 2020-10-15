6504f4dd2606fd90ce086e75ca946208ac53738d
Pat Owens <pat.owens@gmail.com>
2020-10-14 18:37:54 -0700

    Update play-on-scan.js

--- play-on-scan.js
+++ play-on-scan.js
@@ -4,12 +4,23 @@ AFRAME.registerComponent('play-on-scan',
 
     var el = this.el; 
     var currentVideo;
+    var currentAudio;
     el.sceneEl.addEventListener('markerFound', function (ev) { // 
       currentVideo = ev.srcElement.querySelector("a-video");
+      currentAudio = ev.srcElement.querySelector("a-sound");
+
       if(currentVideo && currentVideo.getAttribute("src")){
         var video_el = document.querySelector(currentVideo.getAttribute("src"));
         video_el.play();
       }
+      if(currentAudio && currentAudio.getAttribute("src")){
+        var audio_el = document.querySelector(currentAudio.getAttribute("src"));
+        audio_el.play();
+      }
+    }); 
+
+    el.sceneEl.addEventListener('markerFound', function (ev) { // 
+      
     }); 
 
     /*el.sceneEl.addEventListener('markerLost', function (ev) { // 
