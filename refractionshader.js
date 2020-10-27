11a4c2e7699137ff8aea6374ae2fa3d77bc3f382
Pat Owens <pat.owens@gmail.com>
2020-10-27 09:15:30 -0700

    Update refractionshader.js

--- refractionshader.js
+++ refractionshader.js
@@ -1,3 +1,8 @@
+
+// this piece was created by Andreas Cuervo. I've added some minor changes, but all credit belongs to him
+// Check out the original @ https://gist.github.com/AndresCuervo/b4cfdbc46fe056832d822871522ae3bb
+
+
 // Note: requires https://rawgit.com/jeromeetienne/ar.js/master/aframe/build/aframe-ar.js
 // or A-Frame.js + AR.js + THREEx + some other bindings,
 // basically, just use Jerome Etienne's A-Frame AR lib ¯\_(ツ)_/¯ 
@@ -10,15 +15,27 @@
 
 AFRAME.registerComponent('refraction-shader', {
     init: function () {
+      this.prepare = this.prepare.bind(this)
+      var timerId = setInterval( e => {
+        if (!this.el.sceneEl.systems.arjs['_arSession'])
+          return;
+        clearInterval(timerId)
+        this.prepare()
+      }, 1000)
+    },
+    prepare: function() {
+      
         const data = this.data;
-
-
-        const vertexShader = `varying vec3 vRefract;
+        /**** SHADER DEFINITIONS *****/
+        const vertexShader = `
+        varying vec3 vRefract;
         uniform float refractionRatio;
+
         void main() {
             vec4 mPosition = modelMatrix * vec4( position, 1.0 );
             vec3 nWorld = normalize( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );
             vRefract = normalize( refract( normalize( mPosition.xyz - cameraPosition ), nWorld, refractionRatio ) );
+
             gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
         }`
 
@@ -26,13 +43,19 @@ AFRAME.registerComponent('refraction-shader', {
         varying vec3 vRefract;
         // experiment with distance to the video plane. should do real ray-plane-intersection!
         uniform float distance;
+
         void main(void) {
             vec2 p = vec2(vRefract.x*distance + 0.5, vRefract.y*distance + 0.5);
+
             vec3 color = texture2D( texture, p ).rgb;
             gl_FragColor = vec4( color, 1.0 );
         }`
-        var texture = new THREE.VideoTexture(this.el.sceneEl.systems.arjs.arToolkitSource.domElement)
+        
+        
+        // use the video element to create a video texture     
+        var texture = new THREE.VideoTexture(this.el.sceneEl.systems.arjs['_arSession'].arSource.domElement)
         texture.minFilter =  THREE.NearestFilter
+      
 
         this.material  = new THREE.ShaderMaterial({
             uniforms: {
@@ -55,6 +78,7 @@ AFRAME.registerComponent('refraction-shader', {
         this.material.uniforms.texture.value.wrapS = this.material.uniforms.texture.value.wrapT = THREE.ClampToEdgeWrapping;
         this.applyToMesh();
         this.el.addEventListener('model-loaded', () => this.applyToMesh());
+        this.ready = true
     },
     /**
      * Apply the material to the current entity.
@@ -69,7 +93,8 @@ AFRAME.registerComponent('refraction-shader', {
      * On each frame, update the 'time' uniform in the shaders.
      */
     tick: function (t) {
+      if (this.ready) {
         this.material.uniforms.time.value = t / 1000;
+      }
     }
-
 })
