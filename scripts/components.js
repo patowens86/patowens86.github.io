95a5e6a1d0daee940474d005c94c2d5cc17217ae
Pat Owens <pat.owens@gmail.com>
2020-11-12 08:46:27 -0800

    Update components.js

--- scripts/components.js
+++ scripts/components.js
@@ -41,6 +41,7 @@ AFRAME.registerComponent('start-animation', {
 
     el.addEventListener('click', (e) => {     
       selfModel.setAttribute('visible', 'false')
+      console.log(this.data.self + " is now not visible")
       model.setAttribute('visible','true')
       console.log("Make " + this.data.name + " visible, right?")
       model.setAttribute('scale','2 2 2')
