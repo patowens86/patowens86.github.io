f5d25e9f21386fca2263669c522d43c24ff1ade4
Pat Owens <pat.owens@gmail.com>
2020-11-16 18:26:04 -0800

    revised play-on-scan

---
+++ play-on-scan.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent('play-on-scan', 
+{ 
+  init: function () { 
+
+    var el = this.el; 
+    var currentVideo;
+    el.sceneEl.addEventListener('markerFound', function (ev) { // 
+      currentVideo = ev.srcElement.querySelector("a-video");
+      if(currentVideo && currentVideo.getAttribute("src")){
+        var video_el = document.querySelector(currentVideo.getAttribute("src"));
+        video_el.play();
+      }
+    }); 
+
+    el.sceneEl.addEventListener('markerLost', function (ev) { // 
+      var childVideo = ev.srcElement.querySelector("a-video");
+      if(childVideo && childVideo.getAttribute("src")){
+        var video_el = document.querySelector(el.getAttribute("src"));
+        video_el.pause();
+        video_el.currentTime = 0;
+      }
+    });
+  } 
+});
