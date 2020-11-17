2fa5ad3e7d6c2c14d569c5e8438903fd0dc01466
Pat Owens <pat.owens@gmail.com>
2020-11-16 17:45:53 -0800

    renamed components v8

---
+++ scripts/components_v8.js
@@ -0,0 +1,59 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+AFRAME.registerComponent('navigate-on-click', {
+  schema: {
+    url: {
+      default: ''
+    }
+  },
+  init: function () {
+    console.log("hello")
+    console.log(this.el)
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener('click', function () {
+      //window.location.href = data.url;
+      window.open(data.url, '_blank');
+    });
+  }
+});
+
+AFRAME.registerComponent('color-randomizer', {
+  init: function () {
+    let colors = ["red", "green", "blue", "black", "orange", "white"]
+    var el = this.el;
+    el.addEventListener('click', (e) => {     
+      this.el.setAttribute('color', colors[Math.floor(Math.random() * colors.length)])
+    });
+  }
+});
+
+AFRAME.registerComponent('start-animation', {
+  schema: {
+    name: {type: 'string'},
+  },
+  init: function () {
+    var el = this.el;
+    var model = document.getElementById(this.data.name)
+    var giftArray = document.getElementsByClassName("gift")
+    var sceneArray = document.getElementsByClassName("scene")
+
+
+    el.addEventListener('click', (e) => {
+      for (const scene of sceneArray) {
+        console.log("Scene " + scene)
+        scene.setAttribute('visible', false)
+      }
+      for (const gift of giftArray) {
+        console.log("Scene " + gift)
+        gift.setAttribute('visible', true)
+      }     
+      this.el.setAttribute('visible', false)
+      console.log(this.el.name + " is not visible")
+      model.setAttribute('visible', true)
+      console.log(this.data.name + " is visible")
+
+    });
+  }
+});
