93df202155f82b9e05ed9ad914913b663fb087a7
Pat Owens <pat.owens@gmail.com>
2020-11-22 11:30:45 -0800

    click from camera

--- scripts/components_v8_click.js
+++ scripts/components_v8_click.js
@@ -12,13 +12,16 @@ AFRAME.registerComponent('start-animation', {
     var isActive = false
 
     el.addEventListener('click', (e) => {
+      console.log("Click registered")
        if (!isActive) {
         model.setAttribute('visible', true)
         isActive=true 
+              console.log("Model visible")
        }
        else {
         model.setAttribute('visible', false) 
         isActive=false
+                      console.log("Model not visible")
        }
     })
   }
