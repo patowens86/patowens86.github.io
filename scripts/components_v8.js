4a294d6f83da84e6601aaa9079b101745d56d567
Pat Owens <pat.owens@gmail.com>
2020-12-06 09:40:23 -0800

    Added SantaSelfie placeholder

--- scripts/components_v8.js
+++ scripts/components_v8.js
@@ -70,6 +70,7 @@ AFRAME.registerComponent('start-animation', {
     var scan = true
     var animationLength = this.data.length
     var isSelfie = this.data.selfie
+    var experienceHasStarted = false
     console.log("Selfie state is = " + isSelfie)
 
 
@@ -80,6 +81,10 @@ AFRAME.registerComponent('start-animation', {
       else { scanInstructions.style.display = 'none'}
 
       el.addEventListener('click', (e) => {
+        if(!experienceHasStarted) {
+          //song.components.sound.playSound();
+          experienceHasStarted = true;
+        }
         if(sceneAudio!=null) {
           for (const audio of audioArray) {
             audio.components.sound.pauseSound();
@@ -135,6 +140,9 @@ AFRAME.registerComponent('start-animation', {
     function startSelfie(){
       // xblink_widget.classList.add('xblink--active')
       photoFrame.style.display = "block"
+      console.log("setting selfie mode to true")
+      document.querySelector("a-scene").setAttribute('selfieMode', "true")
+      console.log(document.querySelector("a-scene").getAttribute('selfieMode'))
       shutterButton.hidden = false
       closeButton.hidden = false
       for (const scene of sceneArray) {
@@ -228,7 +236,9 @@ AFRAME.registerComponent('start-animation', {
           photoFrame.style.display = "none" 
 
        }
+       document.querySelector("a-scene").setAttribute('selfieMode', "false")
        cursor.setAttribute('cursor', 'rayOrigin: mouse; fuse: true')
+
     }
   }
 });
@@ -329,8 +339,10 @@ AFRAME.registerComponent('tap-anywhere', {
 AFRAME.registerComponent('photo-mode', {
   schema: {
     name: {type: 'string'},
+    selfieMode: {default: false}
   },
   init: function() {
+    var el = this.el
     const container = document.getElementById('photoModeContainer')
     const image = document.getElementById('photoModeImage')
     const shutterButton = document.getElementById('shutterButton')
@@ -345,7 +357,9 @@ AFRAME.registerComponent('photo-mode', {
     const keepButton = document.getElementById('keepButton')
     const discardButton = document.getElementById('discardButton')
     const photoFrame = document.getElementById('selfieContainer')
+    // let selfieMode = false
     var photoHasBeenTaken = false;
+    //var selfieMode = this.data.selfieMode
     //const overlay = document.getElementById('photoOverlay')
     
     let shareFile
@@ -490,8 +504,8 @@ AFRAME.registerComponent('photo-mode', {
         closeButton.addEventListener('click', promptKeep)
         let sceneHeight = $(window).height()
         let sceneWidth = $(window).width()
-        //console.log(sceneHeight)
-        //console.log(sceneWidth)
+        console.log(sceneHeight)
+        console.log(sceneWidth)
         // let sceneHeightHalf = sceneHeight/2
         // let sceneWidthHalf = sceneWidth/2
               // Show the photo
@@ -506,79 +520,87 @@ AFRAME.registerComponent('photo-mode', {
           width: sceneWidth * 3,
           height: sceneHeight * 3
         })
-        //console.log(scene.components.screenshot.height + ' ' + scene.components.screenshot.width)
+        console.log(scene.components.screenshot.height + ' ' + scene.components.screenshot.width)
         let aScene = scene.components.screenshot.getCanvas("perspective");
-        //console.log("ascene height: " + aScene.height)
-        //console.log("ascene width: " + aScene.width)
+        console.log("ascene height: " + aScene.height)
+        console.log("ascene width: " + aScene.width)
         aSceneWidth = aScene.width;
         aSceneHeight = aScene.height;
         let aSceneOrig = document.querySelector("a-canvas")
-        let santaSelfie = document.getElementById("santaSelfie")
+        //let santaSelfie = document.getElementById("santaSelfie")
         // let actualFrameHeight = 0
         //console.log("capturing video frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
         let frame = captureVideoFrame("video", "jpeg");
-        // let selfieContainer = document.getElementById('selfieContainer')
+        let selfieContainer = document.getElementById('selfieContainer')
 
 
 
-        // console.log("window height: " + sceneHeight)
-        // console.log("window width: " + sceneWidth)
-        // console.log("frame height: " + frame.height)
-        // console.log("frame width: " + frame.width)
-        // //console.log("frame width: " + frame.style.width)
+
+        console.log("window height: " + sceneHeight)
+        console.log("window width: " + sceneWidth)
+        console.log("frame height: " + frame.height)
+        console.log("frame width: " + frame.width)
+        //console.log("frame width: " + frame.style.width)
         // console.log("actual frame height: " + actualFrameHeight)
         //console.log("resizing ascene canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
         aScene = resizeCanvas(aScene, sceneWidth*2, sceneHeight)
         //console.log("finished ascene resizing canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
         aSceneWidth = frame.height*(aSceneWidth/aSceneHeight)
         aSceneHeight = frame.height
-        // console.log("adjusted ascene height: " + frame.height)
-        // //console.log("adjusted ascene height: " + aScene.height)
+        console.log("adjusted ascene height: " + frame.height)
+        //console.log("adjusted ascene height: " + aScene.height)
         // console.log("adjusted ascene width: " + (frame.height*(aSceneWidth/aSceneHeight))*.75)
         //console.log("resizing canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
-        santaSelfie = resizeCanvas(santaSelfie, frame.width, frame.height)
+        //var santaSelfie = new Image()
+        var santaSelfie = '/graphics/SantaSelfiepng.png'
+
         //console.log("frinished resizing canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
         frame = frame.dataUri;
 
         photoHasBeenTaken = true;
-         //console.log("merging images at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
-        // if(selfieContainer.style.display == "block") {
-        //     mergeImages([
-        //       {frame, x: (frame.width/2), y: (frame.height/2)},
-        //       {aScene, x: (aScene.width/2), y: (aScene.height/2)},
-        //       '/graphics/SantaSelfiepng.png'], {
-        //       width: frame.width,
-        //       height: frame.height
-        //     }).then(b64 => {
-        //       // Hide the flash
-        //       container.classList.remove('flash')
-        //       // If an error occurs while trying to take the screenshot, e.detail will be empty.
-        //       // We could either retry or return control to the user
-        //       if (!b64) {
-        //         return
-        //       }
-        //       // e.detail is the base64 representation of the JPEG screenshot
-        //       var basestr = b64 //'data:image/jpeg;base64,' + e.detail
-        //       urlToFile(basestr, 'Merry-Christmas.jpg')
-        //         .then(res => {
-        //           shareFile = res
-        //           imageUrl = URL.createObjectURL(res)
-        //         }).then(() => {
-        //           image.src = imageUrl
-        //         })
+         // console.log("merging images at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
+        console.log(el.getAttribute('selfieMode'))
+        if(el.getAttribute('selfieMode')) {
+          console.log("merging with selfie frame")
+            mergeImages( [
+              {src: frame, x: -(frame.width/3), y: 0},
+              {src: aScene, x: -(sceneWidth/2), y: 0}, 
+              //TODO figure out how to get the selfie to show up correctly
+              {src: santaSelfie, x: 0, y: (frame.height-721)}], {//, '/graphics/SantaSelfie.gif'], {
+              width: sceneWidth,
+              height: frame.height, 
+              quality: 1
+            }).then(b64 => {
+              // Hide the flash
+              //container.classList.remove('flash')
+              // If an error occurs while trying to take the screenshot, e.detail will be empty.
+              // We could either retry or return control to the user
+              if (!b64) {
+                return
+              }
+              // e.detail is the base64 representation of the JPEG screenshot
+              var basestr = b64 //'data:image/jpeg;base64,' + e.detail
+              urlToFile(basestr, 'Merry-Christmas.jpg')
+                .then(res => {
+                  shareFile = res
+                  imageUrl = URL.createObjectURL(res)
+                }).then(() => {
+                  image.src = imageUrl
+                })
                 
               
-        //       // Show the photo
-        //       container.classList.add('photo')
-        //       canvas.classList.add('blur')
-        //       audioButton.style.display = 'none'
-        //       photoFrame.style.display = 'none'
+              // Show the photo
+              container.classList.add('photo')
+              canvas.classList.add('blur')
+              audioButton.style.display = 'none'
+              photoFrame.style.display = 'none'
               
-        //       // Tell the restart-camera script to start watching for issues
-        //       window.dispatchEvent(new Event('ensurecamerastart'))
-        //       // console.log(b64)
-        //     });
-        // } else {
+              // Tell the restart-camera script to start watching for issues
+              window.dispatchEvent(new Event('ensurecamerastart'))
+              // console.log(b64)
+            });
+        } else {
+          console.log("merging without selfie frame")
             mergeImages( [
               {src: frame, x: -(frame.width/3), y: 0},
               {src: aScene, x: -(sceneWidth/2), y: 0}, 
@@ -588,7 +610,7 @@ AFRAME.registerComponent('photo-mode', {
               quality: 1
             }).then(b64 => {
               // Hide the flash
-              container.classList.remove('flash')
+              //container.classList.remove('flash')
               // If an error occurs while trying to take the screenshot, e.detail will be empty.
               // We could either retry or return control to the user
               if (!b64) {
@@ -612,7 +634,8 @@ AFRAME.registerComponent('photo-mode', {
               //Show the b64 data
               ////console.log(b64)
             });
-            $('.flash').fadeOut(300).css({'opacity': 1});// }
+            }// $('.flash').fadeOut(300).css({'opacity': 1}); }
+                        $('.flash').fadeOut(300).css({'opacity': 1}); 
 
 
         
@@ -624,15 +647,15 @@ AFRAME.registerComponent('photo-mode', {
             video = document.getElementById('arjs-video');
             videoStyleWidth = parseInt(video.style.width , 10)
             videoStyleHeight = parseInt(video.style.height, 10)
-            // console.log("video style width: " + videoStyleWidth)
-            // console.log("video style height: " + videoStyleHeight)
+            console.log("video style width: " + videoStyleWidth)
+            console.log("video style height: " + videoStyleHeight)
         // }
         // format = format || 'jpeg';
  
         // if (!video || (format !== 'png' && format !== 'jpeg')) {
         //     return false;
         // }
-        // console.log("video widht: " + video.videoWidth + "  Video height: " + video.videoHeight)
+        console.log("video widht: " + video.videoWidth + "  Video height: " + video.videoHeight)
         var canvas = document.createElement("CANVAS");
         canvas.width = width || videoStyleWidth
         canvas.height = height || videoStyleHeight
