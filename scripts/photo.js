7a0519726020683348bc69179c396c289551c521
Pat Owens <pat.owens@gmail.com>
2020-12-16 10:23:06 -0800

    photo 1.011

--- scripts/photo.js
+++ scripts/photo.js
@@ -752,7 +752,7 @@ AFRAME.registerComponent('photo-mode', {
         //console.log("frame width: " + frame.style.width)
         // console.log("actual frame height: " + actualFrameHeight)
         //console.log("resizing ascene canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
-        aScene = resizeCanvas(aScene, 960, 1280)
+        aScene = resizeCanvas(aScene, 1280, 1280)
         // aScene = resizeCanvas(aScene, sceneWidth*2, sceneHeight)
         //console.log("finished ascene resizing canvas frame at " + new Date().toLocaleTimeString() + " ." + new Date().getMilliseconds())
         aSceneWidth = frame.height*(aSceneWidth/aSceneHeight)
